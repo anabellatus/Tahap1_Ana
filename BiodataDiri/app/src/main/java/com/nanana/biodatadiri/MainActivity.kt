5ad21529b5f9b114c864c621bdaac335045b45c9
ana bellatus <anabellatus@gmail.com>
2020-05-03 14:01:58 +0700

    (+) profile (+) editprofile

--- BiodataDiri/app/src/main/java/com/nanana/biodatadiri/MainActivity.kt
+++ BiodataDiri/app/src/main/java/com/nanana/biodatadiri/MainActivity.kt
@@ -1,12 +1,79 @@
 package com.nanana.biodatadiri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var namaInput: String = ""
+    private var emailInput: String = ""
+    private var telpInput: String = ""
+    private var alamatInput: String = ""
+    private var genderInput: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setDataSpinnerGender()
+
+        btnSave.setOnClickListener {
+            validasiInput()
+        }
     }
+
+    private fun validasiInput() {
+        namaInput = edtName.text.toString()
+        emailInput = edtEmail.text.toString()
+        telpInput = edtTelp.text.toString()
+        alamatInput = edtAddress.text.toString()
+        genderInput = spinnerGender.selectedItem.toString()
+
+        when {
+            namaInput.isEmpty() -> edtName!!.error = "Nama tidak boleh kosong"
+            genderInput.equals("Pilih Jenis Kelamin", ignoreCase = true ) -> tampilToast("Jenis Kelamin harus dipilih")
+            emailInput.isEmpty() -> edtEmail!!.error = "Email tidak boleh kosong"
+            telpInput.isEmpty() -> edtTelp!!.error = "Telp tidak boleh kosong"
+            alamatInput.isEmpty() -> edtAddress.error = "Alamat tidak boleh kosong"
+            else -> {
+                tampilToast("Navigasi ke halaman profil")
+                goToProfileActivity()
+            }
+        }
+    }
+
+    private fun tampilToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun goToProfileActivity() {
+
+        val intent = Intent(this, ProfileActivity::class.java)
+
+        val bundle = Bundle()
+        bundle.putString("nama", namaInput)
+        bundle.putString("gender", genderInput)
+        bundle.putString("email", emailInput)
+        bundle.putString("telp", telpInput)
+        bundle.putString("alamat", alamatInput)
+
+        intent.putExtras(bundle)
+
+        startActivity(intent)
 }
+
+    private fun setDataSpinnerGender() {
+        val adapter = ArrayAdapter.createFromResource(
+            this, R.array.jenis_kelamin, android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerGender.adapter = adapter
+        }
+    }
--- BiodataDiri/app/src/main/java/com/nanana/biodatadiri/ProfileActivity.kt
+++ BiodataDiri/app/src/main/java/com/nanana/biodatadiri/ProfileActivity.kt
@@ -1,12 +1,77 @@
 package com.nanana.biodatadiri
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
+
+    companion object {
+        val REQUEST_CODE = 100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        ambilData()
+
+        btnEditName.setOnClickListener {
+            navigasiKeEditProfile()
+        }
+
+        btnCall.setOnClickListener {
+            dialPhoneNumber(txtTelp.text.toString())
+        }
+    }
+
+    private fun dialPhoneNumber(phoneNumber: String){
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (intent.resolveActivity(packageManager) != null){
+            startActivity(intent)
+        }
+    }
+
+    private fun navigasiKeEditProfile(){
+        val intent = Intent(this, EditProfileActivity::class.java)
+
+        val namaUser = txtName.text.toString()
+        intent.putExtra("nama", namaUser)
+
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK){
+                val result = data?.getStringExtra("nama")
+                txtName.text = result
+            }else{
+                Toast.makeText(this, "Edit Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun ambilData() {
+
+        val bundle = intent.extras
+
+        val nama = bundle!!.getString("nama")
+        val gender = bundle.getString("gender")
+        val email = bundle.getString("email")
+        val telp = bundle.getString("telp")
+        val alamat = bundle.getString("alamat")
+
+        txtName.text = nama
+        txtGender.text = gender
+        txtEmail.text = email
+        txtTelp.text = telp
+        txtAddress.text = alamat
     }
 }
